52fb9c96d2865a3bafe173e97c9977596e06d0e9
gurland <stas.gurland@gmail.com>
2023-02-01 19:45:10 +0200

    fix: make script injectable multiple types by anonimizing it

--- chrome_extension/inject.js
+++ chrome_extension/inject.js
@@ -1,30 +1,15 @@
-const API_BASE = "http://23.88.117.114/api"
+;(function() {
 
-const escapeHtml = (unsafeHTML) => {
+  const escapeHtml = (unsafeHTML) => {
     return unsafeHTML.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
-}
+  }
 
-async function sendToSummarize() {
-  document.body.style.backgroundColor = 'red';
-  const html = escapeHtml(document.documentElement.outerHTML);
-  const url = window.location.href;
-
-  const response = await fetch(API_BASE+"/summaries", {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(
-      {"url": url, "html": html}
-    )
-  });
-
-  return await response.json();
-}
-
-sendToSummarize();
+  const getPageInfo = () => {
+    return {
+      "html": escapeHtml(document.documentElement.outerHTML),
+      "url": window.location.href
+    }
+  };
+  alert("started");
+  return getPageInfo();
+})();
--- chrome_extension/popup/gui.js
+++ chrome_extension/popup/gui.js
@@ -1,13 +1,30 @@
-"use strict";
+const API_BASE = "http://23.88.117.114/api"
 
 const summarizeButton = document.getElementById("summarize");
 summarizeButton.addEventListener("click", async () => {
   console.log("ababa")
-  chrome.tabs.query({active: true, currentWindow: true}, function (result) {
-    result.forEach(function (tab) {
-      console.log(tab);
-      chrome.tabs.executeScript(tab.id, {file: 'inject.js'}, function() {
-        console.log('Successfully injected script into the page');
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    tabs.forEach(function (tab) {
+      chrome.tabs.executeScript(tab.id, {file: 'inject.js'}, async function (pageInfo) {
+        console.log(pageInfo);
+        const response = await fetch(API_BASE + "/summaries", {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          redirect: 'follow',
+          referrerPolicy: 'no-referrer',
+          body: JSON.stringify(
+            pageInfo[0]
+          )
+        });
+        let resp = await response.json();
+        alert(JSON.stringify(resp));
+
+        return resp;
       });
     });
   });
